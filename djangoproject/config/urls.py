8d5e8cf8e9a98bd0b8ad04bb50d034de1305e8fa
JustinnDev <justinalmaodev@gmail.com>
2025-07-02 19:38:02 -0400

    Update urls.py

--- djangoproject/config/urls.py
+++ djangoproject/config/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('landing.urls')),
@@ -28,3 +30,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+urlpatterns += staticfiles_urlpatterns()
