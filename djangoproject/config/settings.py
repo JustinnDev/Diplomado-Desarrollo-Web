eb132a31e571217fbe06a5ac41cbd88ff53c757a
JustinnDev <justinalmaodev@gmail.com>
2025-07-02 22:40:49 -0400

    Update settings.py

--- djangoproject/config/settings.py
+++ djangoproject/config/settings.py
@@ -120,12 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = '/home/justinalmao/Diplomado-Desarrollo-Web/djangoproject/staticfiles'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
