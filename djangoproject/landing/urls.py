32eaa1b009d03c79f1f30f4bd505502360bd3b51
JustinnDev <justinalmaodev@gmail.com>
2025-07-20 23:05:41 -0400

    Landing Listo

--- djangoproject/landing/urls.py
+++ djangoproject/landing/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'landing'
 
 urlpatterns = [
-    path('' , views.index , name='index'),    
+    path('' , views.index , name='index'),
+    path('material1/' , views.material1 , name='material1'),
+    path('material2/' , views.material2 , name='material2'),    
 ]
--- djangoproject/landing/views.py
+++ djangoproject/landing/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'landing/index.html')
+
+def material1(request):
+    return render(request, 'landing/material1.html')
+
+def material2(request):
+    return render(request, 'landing/material2.html')
